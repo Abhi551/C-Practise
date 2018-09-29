7c55ba574ec8e6fe3f979cc027553a63440a58a5
Abhi551 <abhichauhan551@gmail.com>
2018-09-29 10:10:53 +0530

    switch in c++

---
+++ switch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 2;
+
+	switch (a)
+		{
+			case 1:
+				std::cout<<"hello";
+				break;
+			case 2:
+				std::cout<<"end";
+				break;
+
+		}
+	return 0;
+}
