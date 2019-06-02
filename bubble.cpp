ada453eb7419634f2b390d8cec8f2aa86b6f4647
Abhi551 <abhichauhan551@gmail.com>
2019-06-02 23:52:45 +0530

    new bubble sort

--- bubble.cpp
+++ bubble.cpp
@@ -1,47 +1,42 @@
 #include <iostream>
+
 using namespace std;
 
-void swap (int *x , int *y)
+int bubble_sort(int arr[] , int n)
 {
-	int temp = 0;
-	temp = *x;
-	*x = *y;
-	*y = temp;
+	int i,j;
+	int swap = 0;
+	for (i = 0; i<n;  i++)
+	{
+		for (j = 0; j < n-1 ; j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				swap =  arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = swap;
+			}
+		}
+	}
 
+	for (i = 0; i<n; i++)
+	{
+		cout<<arr[i]<<"\n";
+	}
+	return 0;
 }
 
-int main()
-{	int n;
-	int arr[10];
-	int flag;
+main()
+{
+	int arr[10000], n;
+	
+	cout<<"the size of array is = ";
 	cin>>n;
-	for (int i = 0 ; i<n ; i++)
+	for (int i = 0; i<n; i++)
 	{
-		cin>>arr[i];
+		cin>> arr[i];
 	}
 
-	for (int i = 1 ; i<n ; i++)
-	{
-		flag = 0;
-		for (int j = 0 ; j<n-i ; j++)
-		{
-			if (arr[j] > arr[j+1])
-			{	
-				flag = 1;
-				swap(&arr[j] ,  &arr[j+1]);
-			}
+	bubble_sort(arr , n);
 
-		} 
-		if (flag == 0)
-		{
-			break;
-		}
-
-	}
-	for (int i = 0 ; i<n ; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	return 0;
 }
